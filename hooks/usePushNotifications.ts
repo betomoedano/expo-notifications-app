f4cd0532512f46e894dab6eea2f1e969117b426a
betomoedano <betomoedano@outlook.com>
2024-08-26 17:14:46 -0500

    init eas

---
+++ hooks/usePushNotifications.ts
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+import Constants from "expo-constants";
+import { Platform } from "react-native";
+
+const usePushNotifications = () => {
+  const [expoPushToken, setExpoPushToken] = useState<string | undefined>();
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(
+      (token) => setExpoPushToken(token),
+      (error) => setError(error)
+    );
+  }, []);
+
+  const handleRegistrationError = (message: string) => {
+    console.error(message);
+    setError(message);
+  };
+
+  async function registerForPushNotificationsAsync() {
+    if (Platform.OS === "android") {
+      await Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        throw new Error(
+          "Permission not granted to get push token for push notification!"
+        );
+      }
+      const projectId =
+        Constants?.expoConfig?.extra?.eas?.projectId ??
+        Constants?.easConfig?.projectId;
+      if (!projectId) {
+        throw new Error("Project ID not found");
+      }
+      try {
+        const pushTokenString = (
+          await Notifications.getExpoPushTokenAsync({
+            projectId,
+          })
+        ).data;
+        console.log(pushTokenString);
+        return pushTokenString;
+      } catch (e: unknown) {
+        throw new Error(`${e}`);
+      }
+    } else {
+      throw new Error("Must use physical device for push notifications");
+    }
+  }
+
+  return { expoPushToken, error };
+};
+
+export default usePushNotifications;
